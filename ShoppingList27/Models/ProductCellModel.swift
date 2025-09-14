51d98bcbd2ed5f9349523e33fa6f69c599a36711
Hajime4life <doguziagerman@gmail.com>
2025-09-14 13:21:54 +0300

    [Refactor] Removed extra class

--- ShoppingList27/Models/ProductCellModel.swift
+++ ShoppingList27/Models/ProductCellModel.swift
@@ -7,22 +7,6 @@
 
 import SwiftUI
 
-// Это у нас моделька с состоянием
-@Observable
-final class ProductCellModel {
-    var product: Product
-    var isBought: Bool
-    
-    init(model: Product, isBought: Bool = false) {
-        self.product = model
-        self.isBought = isBought
-    }
-    
-    func toggleChecked() {
-        isBought.toggle()
-    }
-}
-
 // Для выбора ед. изм. в дальнейшем
 enum UnitOfMeasure: String, CaseIterable, Identifiable {
     case kilogram = "кг."
@@ -38,13 +22,14 @@ enum UnitOfMeasure: String, CaseIterable, Identifiable {
     var shortName: String { rawValue }
 }
 
-struct Product: Identifiable {
+struct ProductCellModel: Identifiable {
     var id: UUID = UUID()
     var name: String
+    var isBought: Bool
     var count: Int
     var unitMeasure: UnitOfMeasure
     
-    static let mock1 = Product(name: "Чайник", count: 2, unitMeasure: .piece)
-    static let mock2 = Product(name: "Вода", count: 2, unitMeasure: .liter)
-    static let mock3 = Product(name: "Макароны", count: 4, unitMeasure: .mlt)
+    static let mock1 = ProductCellModel(name: "Чайник", isBought: true, count: 2, unitMeasure: .piece)
+    static let mock2 = ProductCellModel(name: "Вода", isBought: false, count: 2, unitMeasure: .liter)
+    static let mock3 = ProductCellModel(name: "Макароны", isBought: false, count: 4, unitMeasure: .mlt)
 }
