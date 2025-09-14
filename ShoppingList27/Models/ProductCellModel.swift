26b120e6eb88cb88243cb0fe70df54c3e0ae62a6
Hajime4life <doguziagerman@gmail.com>
2025-09-14 14:28:45 +0300

    [Refactor] Struct ProductCellModel now is - class ProductModel

--- ShoppingList27/Models/ProductCellModel.swift
+++ ShoppingList27/Models/ProductCellModel.swift
@@ -6,6 +6,7 @@
 //
 
 import SwiftUI
+import Observation
 
 // Для выбора ед. изм. в дальнейшем
 enum UnitOfMeasure: String, CaseIterable, Identifiable {
@@ -22,14 +23,22 @@ enum UnitOfMeasure: String, CaseIterable, Identifiable {
     var shortName: String { rawValue }
 }
 
-struct ProductCellModel: Identifiable {
+@Observable
+final class ProductModel: Identifiable {
     var id: UUID = UUID()
     var name: String
     var isBought: Bool
     var count: Int
     var unitMeasure: UnitOfMeasure
     
-    static let mock1 = ProductCellModel(name: "Чайник", isBought: true, count: 2, unitMeasure: .piece)
-    static let mock2 = ProductCellModel(name: "Вода", isBought: false, count: 2, unitMeasure: .liter)
-    static let mock3 = ProductCellModel(name: "Макароны", isBought: false, count: 4, unitMeasure: .mlt)
+    init(name: String, isBought: Bool, count: Int, unitMeasure: UnitOfMeasure) {
+        self.name = name
+        self.isBought = isBought
+        self.count = count
+        self.unitMeasure = unitMeasure
+    }
+    
+    @MainActor static let mock1 = ProductModel(name: "Чайник", isBought: true, count: 2, unitMeasure: .piece)
+    @MainActor static let mock2 = ProductModel(name: "Вода", isBought: false, count: 2, unitMeasure: .liter)
+    @MainActor static let mock3 = ProductModel(name: "Макароны", isBought: false, count: 4, unitMeasure: .mlt)
 }
