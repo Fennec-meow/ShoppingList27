0abc141478693c382e63c7e5861dd6d08320c5ef
septenmale <zavhorodnii.viktor@icloud.com>
2025-09-14 13:27:57 +0200

    [Refactor] adjust DesignSelector properties

---
+++ ShoppingList27/Screens/ListEditorView/ListEditorView.swift
@@ -0,0 +1,18 @@
+//
+//  ListEditorView.swift
+//  ShoppingList27
+//
+//  Created by Viktor Zavhorodnii on 14/09/2025.
+//
+
+import SwiftUI
+
+struct ListEditorView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ListEditorView()
+}
