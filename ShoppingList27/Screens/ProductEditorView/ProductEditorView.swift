37b2e97e9648a6c7d86893f1f9f636aeead68c41
Vladimir <bizin.vladimir01@gmail.com>
2025-09-19 15:50:10 +0300

    [Feature] add ProductsEditorView

---
+++ ShoppingList27/Screens/ProductEditorView/ProductEditorView.swift
@@ -0,0 +1,167 @@
+//
+//  ProductEditorView.swift
+//  ShoppingList27
+//
+//  Created by Vladimir on 19.09.2025.
+//
+
+import SwiftUI
+
+struct ProductEditorView: View {
+    
+    @Environment(\.dismiss) private var dismiss
+    @State private var productName: String = ""
+    @State private var count: Int?
+    @State private var unit: UnitOfMeasure = .piece
+    private let saveAction: (String, Int, UnitOfMeasure) -> Void
+    
+    private var isSaveEnabled: Bool {
+        !productName.isEmpty && count != nil && count != 0
+    }
+    
+    private let pageTitle: String
+    
+    var body: some View {
+        ZStack {
+            Color.backgroundScreen
+                .ignoresSafeArea()
+            content
+        }
+    }
+    
+    private var content: some View {
+        VStack(spacing: 12) {
+            dragIndicator
+            topBar
+            propertiesSetterView
+            Spacer()
+        }
+        .padding(.top, 4)
+    }
+    
+    private var propertiesSetterView: some View {
+        VStack(spacing: 20) {
+            productNameTextField
+            HStack(spacing: 16) {
+                countTextField
+                UnitsPickerView(unit: $unit)
+            }
+        }
+        .padding(.horizontal, 16)
+        .padding(.vertical, 8)
+    }
+    
+    private var productNameTextField: some View {
+        BaseTextField("Product Name",
+                      text: $productName,
+                      placeholder: "Название товара",
+                      keyboardType: .default)
+    }
+    
+    private var countTextField: some View {
+        BaseTextField("Product Count",
+                      value: $count,
+                      format: .number,
+                      placeholder: "Количество",
+                      keyboardType: .numberPad)
+    }
+    
+    private var unitTextField: some View {
+        EmptyView()
+    }
+    
+    private var topBar: some View {
+        HStack {
+            cancelButton
+                .padding(.horizontal, 16)
+            Spacer()
+            pageTitleView
+            Spacer()
+            doneButton
+                .padding(.horizontal, 16)
+        }
+    }
+    
+    private var pageTitleView: some View {
+        Text(pageTitle)
+            .foregroundStyle(.grey80)
+            .font(.Headline.semiBold)
+    }
+    
+    private var cancelButton: some View {
+        Button {
+            dismiss()
+        } label: {
+            Text("Отменить")
+                .font(.Headline.semiBold)
+        }
+        .tint(.uniTurquoise)
+    }
+    
+    private var doneButton: some View {
+        Button {
+            if let count, isSaveEnabled {
+                saveAction(productName, count, unit)
+            }
+            dismiss()
+        } label: {
+            Text("Готово")
+                .font(isSaveEnabled ? .Headline.semiBold : .Headline.semiBold)
+        }
+        .tint(isSaveEnabled ? .uniTurquoise : .hint)
+        .disabled(!isSaveEnabled)
+    }
+    
+    private var dragIndicator: some View {
+        Color.uniGrey
+            .frame(width: 36, height: 5)
+            .clipShape(RoundedRectangle(cornerRadius: 2.5))
+    }
+    
+    init(product: Product?, onSave: @escaping (String, Int, UnitOfMeasure) -> Void) {
+        saveAction = onSave
+        if let product {
+            productName = product.name
+            count = product.count
+            unit = product.unitMeasure
+            pageTitle = "Редактировать"
+        } else {
+            pageTitle = "Создание товара"
+        }
+    }
+    
+}
+
+#Preview("Create New Product") {
+    @Previewable @State var isShown: Bool = false
+    Button("Show modal view") {
+        isShown = true
+    }
+    .sheet(isPresented: $isShown) {
+        ProductEditorView(product: nil) { name, count, unit in
+            let newProduct = Product(name: name,
+                                     count: count,
+                                     unitMeasure: unit,
+                                     isBought: false)
+            print("Save \(newProduct)")
+        }
+    }
+}
+
+#Preview("Edit Product") {
+    @Previewable @State var product = Product(name: "Чайник",
+                                              count: 1,
+                                              unitMeasure: .piece)
+    @Previewable @State var isShown: Bool = false
+    Button("Show modal view") {
+        isShown = true
+    }
+    .sheet(isPresented: $isShown) {
+        ProductEditorView(product: product, onSave: { name, count, unit in
+            product.name = name
+            product.count = count
+            product.unitMeasure = unit
+            print("Save \(product)")
+        })
+    }
+}
