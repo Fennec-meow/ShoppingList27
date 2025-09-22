89baf12c25949c40fecf8dc5eba8f2178a19f61a
Hajime4life <doguziagerman@gmail.com>
2025-09-22 12:35:31 +0300

    [feature] Added theme switch logic

--- ShoppingList27/App/ShoppingList27App.swift
+++ ShoppingList27/App/ShoppingList27App.swift
@@ -10,14 +10,24 @@ import SwiftUI
 @main
 struct ShoppingList27App: App {
     @AppStorage("hasCompleteOnboarding") private var hasCompleteOnboarding = false
+    @AppStorage("ThemeType") private var selectedThemeType: ThemeType = .system
     @StateObject private var listsMainVM = ListsMainViewModel()
+    @Environment(\.colorScheme) private var systemColorScheme
+    
     var body: some Scene {
         WindowGroup {
-            if hasCompleteOnboarding {
-                ListsMainView(viewModel: listsMainVM)
-            } else {
-                WelcomeScreenView(hasCompletedOnboarding: $hasCompleteOnboarding)
+            Group {
+                if hasCompleteOnboarding {
+                    ListsMainView(viewModel: listsMainVM)
+                } else {
+                    WelcomeScreenView(hasCompletedOnboarding: $hasCompleteOnboarding)
+                }
             }
+            .environment(\.colorScheme,
+                  selectedThemeType == .system
+                  ? systemColorScheme
+                  : (selectedThemeType == .light ? .light : .dark)
+            )
         }
     }
 }
